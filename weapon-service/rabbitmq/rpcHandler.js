f4c522a0ee25ed2e01e6735eda037edd80e18fd3
Marcin Strzelec <m.strzelec21052003@gmail.com>
2025-04-28 23:04:01 +0200

    Add rabbitmq to weapon-service

---
+++ weapon-service/rabbitmq/rpcHandler.js
@@ -0,0 +1,20 @@
+const Weapon = require('./models/Weapon');
+
+const weaponRPCHandler = async (message) => {
+    const { action, weaponIds } = JSON.parse(message.content.toString());
+
+    if (action === 'checkWeaponsExist') {
+        if (!weaponIds || !weaponIds.length) return { valid: false };
+        const count = await Weapon.countDocuments({ _id: { $in: weaponIds } });
+        return { valid: count === weaponIds.length };
+    }
+
+    if (action === 'findWeaponsByIds') {
+        const weapons = await Weapon.find({ _id: { $in: weaponIds } });
+        return weapons;
+    }
+
+    throw new Error('Unknown action type');
+};
+
+module.exports = weaponRPCHandler;
--- weapon-service/server.js
+++ weapon-service/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const connectDB = require('./config/db');
 const weaponRoutes = require('./routes/weaponRoutes');
 const cors = require('cors');
+const { connectRabbitMQ } = require('./rabbitmq/connection');
+const { startRPCServer } = require('./rabbitmq/rpcServer');
+const weaponRPCHandler = require('./rabbitmq/rpcHandler');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +13,8 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+await connectRabbitMQ();
+await startRPCServer('weapon_rpc_queue', weaponRPCHandler);
 
 app.get('/health', (req, res) => res.send('OK'));
 
